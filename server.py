d393a8f1e1281ea1e0db76726849cbb7105b89dc
Ivole32 <167101401+Ivole32@users.noreply.github.com>
2025-07-10 00:34:46 +0200

    Added user delete description

--- server.py
+++ server.py
@@ -44,7 +44,7 @@ def get_user_role(required_role: str):
     return Depends(dependency)
 
 #Landing endpoint
-@app.get("/", tags=["General"], description = "The landing endpoint of the API. It returns a message with the documentation link.")
+@app.get("/", tags=["General"], description="The landing endpoint of the API. It returns a message with the documentation link.")
 @limiter.limit("10/minute")
 def landing_page(request: Request):
     return JSONResponse(content={
@@ -53,7 +53,7 @@ def landing_page(request: Request):
         })
 
 # User endpoints
-@app.get("/user/user-info", tags=["User"], description = "This endpoint returns the key owner's user informations.")
+@app.get("/user/user-info", tags=["User"], description="This endpoint returns the key owner's user informations.")
 @limiter.limit("10/minute")
 def user_info(request: Request, user_data = get_user_role("user")):
     return JSONResponse(content={
@@ -62,18 +62,18 @@ def user_info(request: Request, user_data = get_user_role("user")):
     })
 
 # Admin endpoints
-@app.get("/admin/admin-area", tags=["Admin"], description = "A testing endpoint")
+@app.get("/admin/admin-area", tags=["Admin"], description="A testing endpoint")
 @limiter.limit("10/minute")
 def admin_area(request: Request, user_data = get_user_role("admin")):
     return {"message": f"Admin access granted for {user_data['username']} with role {user_data['role']}!"}
 
-@app.get("/admin/users", tags=["Admin"], description = "This endpoint returns a list of all users with there account informations.")
+@app.get("/admin/users", tags=["Admin"], description="This endpoint returns a list of all users with there account informations.")
 @limiter.limit("5/minute")
 def list_users(request: Request, user_data = get_user_role("admin")):
     users = user_db.list_users()
     return {"users": users}
 
-@app.post("/admin/user/create", tags=["Admin"], description= "This endpoint creates a new user with the specified username, role, and optional API key.")
+@app.post("/admin/user/create", tags=["Admin"], description="This endpoint creates a new user with the specified username, role, and optional API key.")
 @limiter.limit("5/minute")
 def create_user(request: Request, username: str, role: UserRole, api_key: str = "", user_data = get_user_role("admin")):
     user = user_db.add_user(username, role, api_key=api_key)
@@ -83,7 +83,7 @@ def create_user(request: Request, username: str, role: UserRole, api_key: str = 
     return {"user": {"username": username, "role": role.value, "api_key": user}}
 
 # User + Admin endpoints
-@app.delete("/user/delete", tags=["Admin", "User"])
+@app.delete("/user/delete", tags=["Admin", "User"], description="A endpoint to delete a user. If you are an admin you can delet all users and if you are a normal user, you can only delete your account")
 @limiter.limit("5/minute")
 def delete_user(request: Request, username: str, user_data = get_user_role("user")):
     if user_data["role"] == "admin":
