e0a1c7b0d73f65ccd3567419f1bb2bd4e6cc62b0
Ivole32 <167101401+Ivole32@users.noreply.github.com>
2025-09-24 15:00:20 +0200

    Testing better look

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request
+from fastapi.openapi.utils import get_openapi
 from slowapi.middleware import SlowAPIMiddleware
 
 from api.endpoints.admin_endpoints import router as admin_router
@@ -9,7 +10,17 @@ from api.endpoints.mixed_endpoints import router as mixed_router
 
 from core_functions.limiter import limiter
 
-app = FastAPI()
+app = FastAPI(
+    title="Linux-API Server",
+    version="1.0.0",
+    swagger_ui_parameters={
+        "docExpansion": "list",
+        "defaultModelsExpandDepth": -1,
+        "displayRequestDuration": True,
+        "filter": True,
+        "syntaxHighlight.theme": "monokai",
+    },
+)
 app.state.limiter = limiter
 app.add_middleware(SlowAPIMiddleware)
 
@@ -25,6 +36,20 @@ async def add_security_headers(request: Request, call_next):
     response.headers["X-XSS-Protection"] = "1; mode=block"
     response.headers["Access-Control-Allow-Methods"] = "GET, POST, DELETE"
     return response
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title=app.title,
+        version=app.version,
+        description=app.description,
+        routes=app.routes,
+    )
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+app.openapi = custom_openapi
 
 app.include_router(unauthenticated_router)
 app.include_router(user_router)
