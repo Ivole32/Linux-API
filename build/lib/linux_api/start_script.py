0c4d3a1b002c4083459fdba967c3b14858507bd1
root <ivo.theis@posteo.de>
2025-09-29 13:18:01 +0000

    Deleted files that are not needed

--- build/lib/linux_api/start_script.py
+++
@@ -1,7 +0,0 @@
-import os
-import subprocess
-
-def main():
-    base_path = os.path.dirname(os.path.abspath(__file__))
-    start_script = os.path.abspath(os.path.join(base_path, "../start.sh"))
-    subprocess.run(["bash", start_script], check=True)
