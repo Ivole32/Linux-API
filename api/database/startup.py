aaab535b73ceea8ceb892895ebddde4b2facda4b
Ivole32 <167101401+Ivole32@users.noreply.github.com>
2026-01-28 20:07:06 +0100

    Possible fix for backup failure

--- api/database/startup.py
+++ api/database/startup.py
@@ -7,13 +7,19 @@ from api.config.config import (
 from api.database.backup import backup_database
 from api.database.migrate import migration_needed, run_alembic_upgrade_head
 
+import os
+from dotenv import load_dotenv
+load_dotenv() # Load .env
+
+database_url = os.getenv("DATABASE_URL")
+
 
 def startup_database():
     needs_migration = migration_needed()
 
     # Backup database if database should be backuped at startup or a migration needs to be applied
     if BACKUP_DATABASE_AT_STARTUP or (needs_migration and BACKUP_DATABASE_BEFORE_MIGRATION and AUTO_MIGRATE_DATABASE_ON_STARTUP):
-        backup_database()
+        backup_database(database_url=database_url)
 
     # migrate if migration is needed AND auto migrate is enabled
     if needs_migration and AUTO_MIGRATE_DATABASE_ON_STARTUP:
