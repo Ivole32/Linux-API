3c835882d2d561e70d225eb454a967ef4b2ab866
Ivole32 <167101401+Ivole32@users.noreply.github.com>
2026-02-02 21:39:33 +0100

    Fixed missing unique attribute for api_key

--- api/database/models/user.py
+++ api/database/models/user.py
@@ -21,7 +21,7 @@ class UserAuth(Base):
         primary_key=True,
         nullable=False
     )
-    api_key_hash = Column(String, nullable=False)
+    api_key_hash = Column(String, nullable=False, unique=True)
 
 class UserPerm(Base):
     __tablename__ = "user_perm"
