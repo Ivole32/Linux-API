8d22102a320009da2bc044f76ae99068f1956f42
Ivole32 <167101401+Ivole32@users.noreply.github.com>
2026-02-04 19:33:06 +0100

    Fixed not-null constraint for user table

--- api/database/models/user.py
+++ api/database/models/user.py
@@ -7,7 +7,7 @@ SCHEMA = "users"
 class User(Base):
     __tablename__ = "user"
     __table_args__ = {"schema": SCHEMA}
-    user_id = Column(UUID(as_uuid=True), primary_key=True)
+    user_id = Column(UUID(as_uuid=True), server_default=text("gen_random_uuid()"), primary_key=True)
     username = Column(String, unique=True, nullable=False)
     created_at = Column(DateTime, server_default=func.now())
     last_login = Column(DateTime)
