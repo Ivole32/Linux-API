7c3f44bbe1398601db58bd3a788af15c6f023443
Ivole32 <167101401+Ivole32@users.noreply.github.com>
2026-02-12 17:31:58 +0100

    Implemented Demo API key file

--- api/database/user_database/user_database.py
+++ api/database/user_database/user_database.py
@@ -504,6 +504,12 @@ class UserDatabase:
     def create_init_user(self) -> None:
         try:
             self.demo_api_key = self.create_user(username="admin", is_admin=True, activate=True)[2] # return API key
+            
+            # Save API key for demo user if DEMO MODE is enabled
+            if DEMO_MODE:
+                with open("../../../.demo_key.txt", "w") as key_file:
+                    key_file.write(self.demo_api_key)
+
             print(f"Init API key for v1 routes: {self.demo_api_key}")
 
             if not DEMO_MODE:
@@ -513,7 +519,11 @@ class UserDatabase:
         except UniqueViolation:
             if DEMO_MODE and not RESET_DATABASE_WHEN_DEMO:
                 logger.critical("When demo mode is enabled but the database is not reseted at startup by default there won't be a API for users to test with. Set RESET_DATABASE_WHEN_DEMO = True or delete admin account manually.")
-            self.demo_api_key = None
+            if DEMO_MODE:
+                with open("../../../.demo_key.txt", "r") as key_file:
+                    self.demo_api_key = key_file.read()
+            else:
+                self.demo_api_key = None
 
         except Exception as e:
             logger.critical(f"Unexpected error while creating init user: {e}")
