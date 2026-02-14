14eab220f90107ee7b26c7f7857a26e2f6614138
Ivole32 <167101401+Ivole32@users.noreply.github.com>
2026-02-14 23:53:57 +0100

    Feat: d9cbd03f18aa7f7b02ca151a64358964e8a6ed7e

--- api/middleware/legacy.py
+++ api/middleware/legacy.py
@@ -1,25 +1,25 @@
 """
 Middleware to add warnings to legacy response headers and return bodies.
 """
-from fastapi import Request, HTTPException
-from fastapi.responses import Response
+from fastapi import Request
+from fastapi.responses import Response, JSONResponse
 from api.config.config import LEGACY_API_PREFIX
 
 def add_legacy_middleware(app):
     @app.middleware("http")
     async def legacy_middleware(request: Request, call_next):
-        response: Response = await call_next(request)
-
         # I saw some error related to a issue I created some time ago (it got closed)
         # Second try to fix Ref
         # Ref: https://github.com/pallets/werkzeug/issues/3063
         # ZAP found it so here is a possible fix with try/except
         try:
+            response: Response = await call_next(request)
+
             request_url = request.url.path
             request_method = request.method
 
         except Exception as e:
-            raise HTTPException(status_code=500, detail="Unexpected error while parsing host header")
+            raise JSONResponse(status_code=400, content={"detail": "Invalid request."})
 
         if request_url.startswith(LEGACY_API_PREFIX) and request_method != "OPTIONS":
 
